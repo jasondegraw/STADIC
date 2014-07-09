d6a0a24d6fb65ab9b891f93d0e28d691e52a2da8
Jason DeGraw <jason.degraw@gmail.com>
2014-07-09 13:35:01 -0400

    Create logging.h

---
+++ lib/logging.h
@@ -0,0 +1,7 @@
+#ifndef LUMBERJACK_H
+#define LUMBERJACK_H
+
+#define ERROR(mesg) std::cerr << "ERROR: " << (mesg) << std::endl;
+#define WARNING(mesg) std::cerr << "WARNING: " << (mesg) << std::endl;
+
+#endif
