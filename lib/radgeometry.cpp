e3375b342f3436149b6951627cfc821445b5dc9b
Jason W. DeGraw <jason.degraw@gmail.com>
2014-07-24 22:19:40 -0400

    Add to incomplete factory method

--- lib/radgeometry.cpp
+++ lib/radgeometry.cpp
@@ -1,6 +1,15 @@
 #include "radgeometry.h"
 
-std::array<QString,50> RadGeometry::s_typeStrings = {"Source", "Sphere", "Bubble", "Polygon", "Cone", "Cup", "Cylinder", "Tube", "Ring", "Instance", "Mesh", "Light", "Illum", "Glow", "Spotlight", "Mirror", "Prism1", "Prism2", "Plastic", "Metal", "Trans", "Plastic2", "Metal2", "Trans2", "Mist", "Dielectric", "Interface", "Glass", "Plasfunc", "Metfunc", "Transfunc", "BRTDfunc", "Plasdata", "Metdata", "Transdata", "BSDF", "Antimatter", "Texfunc", "Texdata", "Colorfunc", "Brightfunc", "Colordata", "Brightdata", "Colorpict", "Colortext", "Brighttext", "Mixfunc", "Mixdata", "Mixpict", "Mixtext"};
+std::array<QString,51> RadGeometry::s_typeStrings = {"source", "sphere", "bubble", "polygon", "cone", "cup",
+                                                     "cylinder", "tube", "ring", "instance", "mesh", "light",
+                                                     "illum", "glow", "spotlight", "mirror", "prism1", "prism2",
+                                                     "plastic", "metal", "trans", "plastic2", "metal2", "trans2",
+                                                     "mist", "dielectric", "interface", "glass", "plasfunc",
+                                                     "metfunc", "transfunc", "brtdfunc", "plasdata", "metdata",
+                                                     "transdata", "bsdf", "antimatter", "texfunc", "texdata",
+                                                     "colorfunc", "brightfunc", "colordata", "brightdata",
+                                                     "colorpict", "colortext", "brighttext", "mixfunc", "mixdata",
+                                                     "mixpict", "mixtext", "unknown"};
 
 RadGeometry::RadGeometry(QObject *parent) :
     QObject(parent)
@@ -15,7 +24,7 @@ void RadGeometry::setModifier(QString modifier){
     m_Modifier=modifier;
 }
 void RadGeometry::setType(QString type){
-    m_Type=type;
+    m_TypeString=type;
 }
 void RadGeometry::setName(QString name){
     m_Name=name;
@@ -34,8 +43,11 @@ void RadGeometry::setArg3(QString arg){
 QString RadGeometry::modifier(){
     return m_Modifier;
 }
-QString RadGeometry::type(){
-    return m_Type;
+RadGeometry::Type RadGeometry::type(){
+    return typeFromString(m_TypeString);
+}
+QString RadGeometry::typeString(){
+    return m_TypeString;
 }
 QString RadGeometry::name(){
     return m_Name;
@@ -50,3 +62,230 @@ std::vector<QString> RadGeometry::arg3(){
     return m_Arg3;
 }
 
+RadGeometry* RadGeometry::fromRad(QFile file, QObject *parent)
+{
+    RadGeometry *obj;
+    QString type;
+    switch(typeFromString(type)) {
+    case RadGeometry::Plastic:
+        obj = new ::Plastic(parent);
+        break;
+    default:
+        obj = new RadGeometry(parent);
+        break;
+    }
+    return obj;
+}
+
+RadGeometry::Type RadGeometry::typeFromString(QString string)
+{
+    for(unsigned i=0;i<s_typeStrings.size();i++) {
+        if(s_typeStrings[i] == string) {
+            return (RadGeometry::Type)i;
+        }
+    }
+    return Unknown;
+}
+
+QString RadGeometry::getArg1(int position) const
+{
+    if(position<0 || position>=m_Arg1.size()) {
+        return QString();
+    }
+    return m_Arg1[position];
+}
+
+QString RadGeometry::getArg2(int position) const
+{
+    if(position<0 || position>=m_Arg2.size()) {
+        return QString();
+    }
+    return m_Arg2[position];
+}
+
+QString RadGeometry::getArg3(int position) const
+{
+    if(position<0 || position>=m_Arg3.size()) {
+        return QString();
+    }
+    return m_Arg3[position];
+}
+
+QString RadGeometry::getArg(int number, int position) const
+{
+    std::vector<QString> *arg;
+    switch(number) {
+    case 1:
+        return getArg1(position);
+    case 2:
+        return getArg2(position);
+    case 3:
+        return getArg3(position);
+    }
+    // Error/warning message?
+    return QString();
+}
+
+bool RadGeometry::setArg(int number, QString value, int position)
+{
+    std::vector<QString> *arg;
+    switch(number) {
+    case 1:
+        arg = &m_Arg1;
+        break;
+    case 2:
+        arg = &m_Arg2;
+        break;
+    case 3:
+        arg = &m_Arg3;
+        break;
+    default:
+        // Error/warning message?
+        return false;
+    }
+    if(validateArg(number,value,position)) {
+        if(position>=arg->size()) {
+            arg->resize(position+1);
+        }
+        (*arg)[position] = value;
+        return true;
+    } else {
+        // Error/warning message?
+    }
+    return false;
+}
+
+void RadGeometry::initArg(int number, std::vector<QString> arg)
+{
+    switch(number) {
+    case 1:
+        m_Arg1 = arg;
+        break;
+    case 2:
+        m_Arg2 = arg;
+        break;
+    case 3:
+        m_Arg3 = arg;
+        break;
+    default:
+        // Error message?
+        break;
+    }
+}
+
+Plastic::Plastic(QObject *parent) : RadGeometry(parent)
+{
+    std::vector<QString> arg3 = {"0","0","0","0","0"};
+    initArg(3,arg3);
+}
+
+Plastic::Plastic(double red, double green, double blue, double spec, double rough, QObject *parent)
+    : RadGeometry(parent)
+{
+    setArg(3,QString().sprintf("%g",red),0);
+    setArg(3,QString().sprintf("%g",green),1);
+    setArg(3,QString().sprintf("%g",blue),2);
+    setArg(3,QString().sprintf("%g",spec),3);
+    setArg(3,QString().sprintf("%g",rough),4);
+}
+
+// Setters
+bool Plastic::setRed(double value)
+{
+    return setArg(3,QString().sprintf("%g",value),0);
+}
+
+bool Plastic::setGreen(double value)
+{
+    return setArg(3,QString().sprintf("%g",value),1);
+}
+
+bool Plastic::setBlue(double value)
+{
+    return setArg(3,QString().sprintf("%g",value),2);
+}
+
+bool Plastic::setSpecularity(double value)
+{
+    return setArg(3,QString().sprintf("%g",value),3);
+}
+
+bool Plastic::setRoughness(double value)
+{
+    return setArg(3,QString().sprintf("%g",value),4);
+}
+
+// Getters
+double Plastic::red() const
+{
+    bool ok;
+    double value = getArg3(0).toDouble(&ok);
+    if(!ok) {
+        // This is bad and should *never* happen
+        // Probably need to issue a panicky error message
+        return 0;
+    }
+    return value;
+}
+
+double Plastic::green() const
+{
+    bool ok;
+    double value = getArg3(1).toDouble(&ok);
+    if(!ok) {
+        // This is bad and should *never* happen
+        // Probably need to issue a panicky error message
+        return 0;
+    }
+    return value;
+}
+
+double Plastic::blue() const
+{
+    bool ok;
+    double value = getArg3(2).toDouble(&ok);
+    if(!ok) {
+        // This is bad and should *never* happen
+        // Probably need to issue a panicky error message
+        return 0;
+    }
+    return value;
+}
+
+double Plastic::specularity() const
+{
+    bool ok;
+    double value = getArg3(3).toDouble(&ok);
+    if(!ok) {
+        // This is bad and should *never* happen
+        // Probably need to issue a panicky error message
+        return 0;
+    }
+    return value;
+}
+
+double Plastic::roughness() const
+{
+    bool ok;
+    double value = getArg3(4).toDouble(&ok);
+    if(!ok) {
+        // This is bad and should *never* happen
+        // Probably need to issue a panicky error message
+        return 0;
+    }
+    return value;
+}
+
+bool Plastic::validateArg(int number, QString value, int position)
+{
+    if(number==3) {
+        if(position>0 && position<5) {
+            bool ok;
+            double dval = value.toDouble(&ok);
+            if(ok && dval >= 0 and dval <= 1.0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
--- lib/radgeometry.h
+++ lib/radgeometry.h
@@ -2,6 +2,7 @@
 #define RADGEOMETRY_H
 
 #include <QObject>
+#include <QFile>
 #include<array>
 
 class RadGeometry : public QObject
@@ -13,7 +14,7 @@ public:
                Trans, Plastic2, Metal2, Trans2, Mist, Dielectric, Interface, Glass, Plasfunc,
                Metfunc, Transfunc, BRTDfunc, Plasdata, Metdata, Transdata, BSDF, Antimatter,
                Texfunc, Texdata, Colorfunc, Brightfunc, Colordata, Brightdata, Colorpict, Colortext,
-               Brighttext, Mixfunc, Mixdata, Mixpict, Mixtext};
+               Brighttext, Mixfunc, Mixdata, Mixpict, Mixtext, Unknown};
     explicit RadGeometry(QObject *parent = 0);
 
     //Setters
@@ -24,21 +25,35 @@ public:
     void setArg2(QString arg);
     void setArg3(QString arg);
 
+    virtual bool setArg(int number, QString value, int position);
+
     //Getters
     QString modifier();
-    QString type();
+    Type type();
+    QString typeString();
     QString name();
     std::vector<QString> arg1();
     std::vector<QString> arg2();
     std::vector<QString> arg3();
 
+    virtual QString getArg1(int position) const;
+    virtual QString getArg2(int position) const;
+    virtual QString getArg3(int position) const;
+    virtual QString getArg(int number, int position) const;
+
+    static RadGeometry *fromRad(QFile file, QObject *parent = 0);
+
+protected:
+    void initArg(int number, std::vector<QString> arg);
 
 private:
-    static std::array<QString,50> s_typeStrings;
+    virtual bool validateArg(int number, QString value, int position) {return true;}
+    static std::array<QString,51> s_typeStrings;
     static Type typeFromString(QString string);
 
     QString m_Modifier;
-    QString m_Type;
+    Type m_Type;
+    QString m_TypeString;
     QString m_Name;
     std::vector<QString> m_Arg1;
     std::vector<QString> m_Arg2;
@@ -48,6 +63,28 @@ signals:
 
 public slots:
 
+};
+
+class Plastic : public RadGeometry
+{
+public:
+    explicit Plastic(QObject *parent=0);
+    Plastic(double red, double green, double blue, double spec, double rough, QObject *parent=0);
+
+    // Setters
+    bool setRed(double value);
+    bool setGreen(double value);
+    bool setBlue(double value);
+    bool setSpecularity(double value);
+    bool setRoughness(double value);
+    // Getters
+    double red() const;
+    double green() const;
+    double blue() const;
+    double specularity() const;
+    double roughness() const;
+private:
+   bool validateArg(int number, QString value, int position);
 };
 
 #endif // RADGEOMETRY_H
