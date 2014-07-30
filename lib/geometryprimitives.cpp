de9152a6da8e53c9964f148e2e44ec8e160479d7
Jason W. DeGraw <jason.degraw@gmail.com>
2014-07-29 23:34:44 -0400

    Add empty geometry primitives files
    Closes #7

---
+++ lib/geometryprimitives.cpp
@@ -0,0 +1,7 @@
+#include "geometryprimitives.h"
+
+namespace stadic {
+
+
+
+}
---
+++ lib/geometryprimitives.h
@@ -0,0 +1,11 @@
+#ifndef GEOMETRYPRIMITIVES_H
+#define GEOMETRYPRIMITIVES_H
+#include "radprimitive.h"
+
+namespace stadic {
+
+
+
+}
+
+#endif // GEOMETRYPRIMITIVES_H
