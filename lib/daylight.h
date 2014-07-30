e94da99b272e8d10042c4bd279f22b268dbea9ae
CraigCasey <c.casey.psu@gmail.com>
2014-07-30 16:45:24 -0400

    Added gen_dc_adv code to daylight.cpp
    closes #10

--- lib/daylight.h
+++ lib/daylight.h
@@ -2,21 +2,32 @@
 #define DAYLIGHT_H
 
 #include <QObject>
+#include "stadiccontrol.h"
+#include <vector>
 
+namespace stadic {
 class daylight : public QObject
 {
     Q_OBJECT
 public:
     explicit daylight(QObject *parent = 0);
-    bool simDaylight(QString fileName);
+    bool simDaylight(stadic::Control &model);
 
 private:
-    bool simBSDF(int blindGroupNum, int setting, QString radFileName, int BSDFNum, QString glassRad, QString bsdfRad);
+    bool simBSDF(int blindGroupNum, int setting, int bsdfNum);
+    bool simStandard(int blindGroupNum, int setting);
+    bool uniqueGlazingMaterials(stadic::Control &model);
+    bool testSimCase(stadic::Control &model);
+    bool setSimCase(int setting, int simCase);
+    bool writeSky(stadic::Control &model);
 
+
+    std::vector<int> m_SimCase;
 signals:
 
 public slots:
 
 };
 
+}
 #endif // DAYLIGHT_H
