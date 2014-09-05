93fe755d20958639326a1ae5a0c68ab5a9453a7f
Jason DeGraw <jason.degraw@gmail.com>
2014-09-05 11:36:08 -0400

    Update logging.cpp

--- lib/logging.cpp
+++ lib/logging.cpp
@@ -2,13 +2,28 @@
 #include <iostream>
 #include <QString>
 
-void ERROR(QString mesg){
+void ERROR(QString mesg)
+{
     QString string;
     string ="ERROR: "+mesg;
     std::cerr << string.toStdString() << std::endl;
 }
-void WARNING(QString mesg){
+
+void WARNING(QString mesg)
+{
     QString string;
     string="WARNING: "+mesg;
     std::cerr << string.toStdString() << std::endl;
+}
+
+void ERROR(std::string mesg)
+{
+    std::string string ="ERROR: "+mesg;
+    std::cerr << string << std::endl;
+}
+
+void WARNING(std::string mesg)
+{
+    std::string string="WARNING: "+mesg;
+    std::cerr << string << std::endl;
 }
