bda6295a6a28ddab227aaaed7042e965b8b2b037
CraigCasey <c.casey.psu@gmail.com>
2014-09-06 12:18:17 -0400

    Added FileSystem object and objectstests
    @JasonDegraw please review and comment

--- lib/objects.cpp
+++ lib/objects.cpp
@@ -1,9 +1,74 @@
 #include "objects.h"
+//#include <sys/types.h>
+#include <sys/stat.h>
+#include "logging.h"
 
 namespace stadic{
 
+//FileSystem
+FileSystem::FileSystem()
+{
+
+}
+//Setters
+void FileSystem::setPath(std::string path){
+    m_Path=path;
+}
+
+//Getters
 
 
+//Utilities
+bool FileSystem::exists(){
+    if(isDir()){
+        return true;
+    }
+    if (isFile()){
+        return true;
+    }
+    return false;
+}
+bool FileSystem::isDir(){
+    struct stat path;
+
+    if (stat(m_Path.c_str(), &path)==0 && S_ISDIR(path.st_mode)){
+        return true;
+    }
+    return false;
+}
+bool FileSystem::isFile(){
+    struct stat path;
+
+    if (stat(m_Path.c_str(), &path)==0 && S_ISREG(path.st_mode)){
+        return true;
+    }
+    return false;
+}
+bool FileSystem::isUpdated(){
+    struct tm* originalMod=m_LastMod;
+    if (!lastMod()){
+        ERROR("There was an error checking the file.");
+        return false;
+    }
+    if (difftime(mktime(m_LastMod),mktime(originalMod))>0){
+        return true;
+    }
+    return false;
+}
+bool FileSystem::lastMod(){
+    struct stat path;
+    if (isFile()){
+        stat(m_Path.c_str(),&path);
+        m_LastMod=localtime(&path.st_mtime);
+    }else{
+        ERROR("The path provided is not a file.");
+        return false;
+    }
+    return true;
+}
+
+
+//Private
 
 
 }
--- lib/objects.h
+++ lib/objects.h
@@ -1,8 +1,33 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
+#include <string>
+#include <time.h>
+#include "stadicapi.h"
+
 
 namespace stadic{
+class STADIC_API FileSystem
+{
+public:
+    FileSystem();
 
+    // Setters
+    void setPath(std::string path);
+    // Getters
+
+    //Utilities
+    bool exists();
+    bool isDir();
+    bool isFile();
+    bool isUpdated();
+    bool lastMod();
+
+private:
+    std::string m_Path;
+    struct tm *m_LastMod;
+
+
+};
 
 
 
