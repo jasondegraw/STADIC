705e2bfbab01511e1e02ba3a480de55f7e7547b7
Jason W. DeGraw <jason.degraw@gmail.com>
2014-09-05 23:07:04 -0400

    Add a QProcess wrapper class and a test
    Also, started changing daylight.cpp to use the class.

--- lib/objects.cpp
+++ lib/objects.cpp
@@ -2,8 +2,62 @@
 
 namespace stadic{
 
+Process::Process()
+{
+}
 
+void Process::start()
+{
+    m_process.start();
+}
 
+void Process::setProgram(std::string prog)
+{
+    m_process.setProgram(QString::fromStdString(prog));
+}
 
+void Process::setArguments(std::vector<std::string> args)
+{
+    QStringList arguments;
+    for(std::string arg : args) {
+        arguments << QString::fromStdString(arg);
+    }
+    m_process.setArguments(arguments);
+}
+
+bool Process::waitForFinished(int msecs)
+{
+    return m_process.waitForFinished(msecs);
+}
+
+std::string Process::error()
+{
+    return QString(m_process.readAllStandardError()).toStdString();
+}
+
+std::string Process::output()
+{
+    return QString(m_process.readAllStandardOutput()).toStdString();
+}
+
+void Process::setStandardOutputProcess(Process *destination)
+{
+    m_process.setStandardOutputProcess(&(destination->m_process));
+}
+
+void Process::setStandardErrorFile(const std::string &fileName)
+{
+    m_process.setStandardErrorFile(QString::fromStdString(fileName));
+}
+
+void Process::setStandardInputFile(const std::string &fileName)
+{
+    m_process.setStandardInputFile(QString::fromStdString(fileName));
+}
+
+void Process::setStandardOutputFile(const std::string &fileName)
+{
+    m_process.setStandardOutputFile(QString::fromStdString(fileName));
+}
 
 }
--- lib/objects.h
+++ lib/objects.h
@@ -1,9 +1,35 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
+#include <QProcess>
+#include <QStringList>
+
+#include "stadicapi.h"
+
 namespace stadic{
 
+class STADIC_API Process
+{
+public:
+  Process();
 
+  void start();
+  void setProgram(std::string prog);
+  void setArguments(std::vector<std::string> args);
+  bool waitForFinished(int msecs=30000); // Yuck.
+
+  std::string error();
+  std::string output();
+
+  void setStandardOutputProcess(Process *destination);
+  void setStandardErrorFile(const std::string &fileName);
+  void setStandardInputFile(const std::string &fileName);
+  void setStandardOutputFile(const std::string &fileName);
+
+private:
+  QProcess m_process;
+
+};
 
 
 
