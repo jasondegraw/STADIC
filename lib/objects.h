e7ac02ec8ffd824f9083704b9eda1543e0387005
Jason W. DeGraw <jason.degraw@gmail.com>
2014-09-06 14:49:24 -0400

    Add in the QProcess code

--- lib/objects.h
+++ lib/objects.h
@@ -3,6 +3,10 @@
 
 #include <memory>
 #include <vector>
+#include <QProcess>
+#include <QStringList>
+
+#include "stadicapi.h"
 
 // This is a C++11 thing - if we can't use this then we're looking
 // at a define or some other horrifying construct
@@ -11,7 +15,28 @@ using shared_vector = std::vector<std::shared_ptr<T> >;
 
 namespace stadic{
 
+class STADIC_API Process
+{
+public:
+  Process();
 
+  void start();
+  void setProgram(std::string prog);
+  void setArguments(std::vector<std::string> args);
+  bool waitForFinished(int msecs=30000); // Yuck.
+
+  std::string error();
+  std::string output();
+
+  void setStandardOutputProcess(Process *destination);
+  void setStandardErrorFile(const std::string &fileName);
+  void setStandardInputFile(const std::string &fileName);
+  void setStandardOutputFile(const std::string &fileName);
+
+private:
+  QProcess m_process;
+
+};
 
 
 
