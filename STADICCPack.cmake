db9566114acc6f9d93b7002bf858f01f3b447649
Jason W. DeGraw <jason.degraw@gmail.com>
2014-09-30 03:23:47 -0400

    Update CPack scripts

--- STADICCPack.cmake
+++ STADICCPack.cmake
@@ -19,14 +19,34 @@ set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE.txt")
 
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Simulation Tool for Architectural Daylighting and Integrated Controls")
 
+set(CPACK_WIX_UPGRADE_GUID 65D84AD9-6793-4656-99F6-592D892A8D58)
+set(CPACK_WIX_UI_BANNER "${CMAKE_SOURCE_DIR}/installer/banner.bmp")
+set(CPACK_WIX_UI_DIALOG "${CMAKE_SOURCE_DIR}/installer/background.bmp")
+
 #set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/CPack.Description.txt")
 #set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/CPack.Description.txt")
 #set(CPACK_RESOURCE_FILE_WELCOME "${CMAKE_CURRENT_SOURCE_DIR}/CPack.Welcome.txt")
 
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "STADIC\n ${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
-#set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5gui5")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5core5")
 #set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "http://psu.edu")
 set(CPACK_DEBIAN_PACKAGE_SECTION "science")
 #set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_CURRENT_SOURCE_DIR}/CMake/debian/postinst")
+
+if(WIN32)
+  set(CPACK_GENERATOR "WIX")
+endif()
+
+# Maybe this should go elsewhere
+
+if(MINGW)
+  get_filename_component(MinGW_Path ${CMAKE_CXX_COMPILER} PATH)
+  set(MINGW_RUNTIME_LIBS ${MinGW_Path}/libgcc_s_dw2-1.dll
+                         ${MinGW_Path}/libstdc++-6.dll
+                         ${MinGW_Path}/libwinpthread-1.dll)
+  set(CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS ${MINGW_RUNTIME_LIBS})
+endif()
+
+include(InstallRequiredSystemLibraries)
+
+include(CPack)
 
