815904aa830322e8fd2fa40af3d6a59347d2aa30
CraigCasey <c.casey.psu@gmail.com>
2014-10-09 21:18:28 -0400

    Fixed copyright symbol
    closes #13

--- lib/stadicprocess.cpp
+++ lib/stadicprocess.cpp
@@ -1,5 +1,5 @@
 /****************************************************************
- * Copyright © 2014, The Pennsylvania State University
+ * Copyright (c) 2014, The Pennsylvania State University
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms,
