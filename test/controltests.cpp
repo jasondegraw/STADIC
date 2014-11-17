3b41d3d40a78b7f06f06883b4c7644b27aeb8b92
Jason W. DeGraw <jason.degraw@gmail.com>
2014-11-16 20:19:29 -0500

    File name clean up

--- test/controltests.cpp
+++ test/controltests.cpp
@@ -44,7 +44,7 @@
 TEST(ControlTests, PTreeReadJson)
 {
   stadic::Control controlFile;
-  ASSERT_TRUE(controlFile.parseJson("TestSControl.json"));
+  ASSERT_TRUE(controlFile.parseJson("control.json"));
   EXPECT_EQ("prj1",controlFile.projectName());
   EXPECT_EQ("c:/example/", controlFile.projectFolder());
   EXPECT_EQ("tmp/", controlFile.tmpFolder());
