0b8d8fb3e0780247438ad17ce0df3e9e7a26c6d4
Jason W. DeGraw <jason.degraw@gmail.com>
2015-02-23 12:40:53 -0700

    Fix formatting errors in process test
    Should be good to go for about 8000 years

--- test/processtests.cpp
+++ test/processtests.cpp
@@ -79,8 +79,12 @@ TEST(ProcessTests, ProcessProgramArgs)
 #ifdef _WIN32
     SYSTEMTIME time;
     GetLocalTime(&time);
-    std::string datestring = days[time.wDayOfWeek] + " " + stadic::toString(time.wMonth) + "/"
-      + stadic::toString(time.wDay) + "/" + stadic::toString(time.wYear);
+    std::stringstream stream;
+    stream << days[time.wDayOfWeek] << " ";
+    stream << std::setw(2) << std::setfill('0') << time.wMonth;
+    stream << "/";
+    stream << std::setw(2) << std::setfill('0') << time.wDay;
+    std::string datestring = stream.str() + "/" + stadic::toString(time.wYear);
     std::vector<std::string> args;
     args.push_back("/c");
     args.push_back("date");
