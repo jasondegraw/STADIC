c8cfcbcff0b050eeb84e882dfaafb40b3036199a
Jason W. DeGraw <jason.degraw@gmail.com>
2015-08-27 11:07:14 -0600

    Fix missing POSIX features in filepath.cpp

--- lib/filepath.cpp
+++ lib/filepath.cpp
@@ -36,20 +36,19 @@
 #include <iostream>
 #ifdef _MSC_VER
 #include <Windows.h>
-#else
+#include <direct.h>
+#else //POSIX
 #include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
 #endif
-
-// May need to fix later for POSIX
-#include <direct.h>
 
 #ifdef _WIN32
 #define PATHSEPARATOR '\\'
 #define MKDIR _mkdir
-#else
+#else //POSIX
 #define PATHSEPARATOR '/'
-#define MKDIR mkdir
+#define MKDIR(dir) mkdir(dir, 0777)
 #endif
 
 namespace stadic{
