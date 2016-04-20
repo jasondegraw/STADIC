f367658858aa21bbfa8af0d7374d4840a6fa22d6
Jason W. DeGraw <jason.degraw@gmail.com>
2016-04-20 12:32:29 -0600

    Yet another try

--- radiance-build.sh
+++ radiance-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pwd
 sed --in-place '/more License.txt/d' makeall
-echo '\ny\n' | sudo ./installib
-echo '\n\ny\n2\n\nn\n' | sudo ./makeall install
+echo '\ny\n' | sudo /bin/sh ./installib
+#echo '\n\ny\n2\n\nn\n' | sudo ./makeall install
