b83f92259653769de6732adf45d23a224d37aa60
Jason W. DeGraw <jason.degraw@gmail.com>
2016-04-20 07:59:11 -0600

    Yet another try to build Radiance

--- radiance-build.sh
+++ radiance-build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-wget http://www.radiance-online.org/download-install/radiance-source-code/latest-release/latest-release/rad5R0all.tar.gz
-tar xvf rad5R0all.tar.gz
-cd ray
 sed --in-place '/more License.txt/d' makeall
 echo '\ny\n' | sudo ./installib
 echo '\n\ny\n2\n\nn\n' | sudo ./makeall install
